4d5c339d59a61ec0f1291f2b16d6ec98ad573ba9
kyunghee kwak <kwakkyunghee325@gmail.com>
2023-04-27 10:18:30 +0900

    spring

---
+++ src/main/java/kr/hs/study/beans/TestBean2.java
@@ -0,0 +1,19 @@
+package kr.hs.study.beans;
+
+import lombok.Data;
+
+@Data
+public class TestBean2 {
+    private DataBean3 data3;
+    private DataBean3 data4;
+
+//    public DataBean3 getData3(){
+//        return data3;
+//    }
+//
+//    public DataBean3 getData4(){
+//        return data4;
+//    }
+
+
+}
